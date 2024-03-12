f94cb551cb1d3fe96607d8cb3a015f2f8b782c51
Kikiriki070204 <kiarabarrientossalazar@gmail.com>
2024-03-12 12:15:54 -0600

    first commit

---
+++ database/migrations/2024_03_12_161212_users.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->unsignedBigInteger('rol_id')->default(2);
+            $table->foreign('rol_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->boolean('active')->default(false);
+            $table->boolean('verified')->default(false);
+            $table->unsignedBigInteger('gimnasio_id')->nullable();
+           $table->foreign('gimnasio_id')->references('id')->on('gimnasios')->onDelete('cascade');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('register',[AuthController::class,'register']);
+Route::get('activate/{user}',[AuthController::class,'activate'])->name('activate')->middleware('signed');
+
+Route::post('login',[AuthController::class,'login'])->name('login');
+Route::post('verify/{id}',[AuthController::class,'verify'])->name('verify');
+
